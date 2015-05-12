ac6ba2a961eddb0fc1669559bf618bd4a386d2a9
Javier Herrero <javiherar@yahoo.com>
2015-05-12 12:20:46 +0200

    Degree Class

---
+++ Module_8_Assignment/Degree.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Description: Module Eight Assignment.
+// Author: Javier Herrero Arnanz.
+
+namespace Module_8_Assignment
+{
+    class Degree
+    {
+        // Degree variables and properties.
+        public string DegreeName { get; set; }
+        public int TotalCredits { get; set; }
+        public Course Course { get; set; }
+
+        // Constructor.
+        public Degree(string dName, int credits, Course course)
+        {
+            this.DegreeName = dName;
+            this.TotalCredits = credits;
+            this.Course = course;
+        }
+    }
+}
